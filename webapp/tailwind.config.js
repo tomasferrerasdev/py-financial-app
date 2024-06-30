6e89ea1bf9dc48118b737fc1eec530d8ffd3784b
Tomas Ferreras <hellotomasdev@gmail.com>
2024-06-30 19:55:28 -0300

    feat: setup tailwind

--- webapp/tailwind.config.js
+++ webapp/tailwind.config.js
@@ -1,9 +1,11 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Source Sans Pro"', "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
