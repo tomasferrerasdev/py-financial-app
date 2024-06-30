bc365825bcffb830e65482ef2eeaca0f622d84fa
Tomas Ferreras <hellotomasdev@gmail.com>
2024-06-30 20:44:07 -0300

    feat: add vite port cors origin

--- backend/main.py
+++ backend/main.py
@@ -11,12 +11,15 @@ from database import SessionLocal, engine
 app = FastAPI()
 origins = [
     "http://localhost:3000",
+    "http://localhost:5173",
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
