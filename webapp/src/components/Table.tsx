80dad3c1ef35d4f19a1568c7654360062fa3096a
Tomas Ferreras <hellotomasdev@gmail.com>
2024-06-30 22:26:16 -0300

    feat: add transactions table

--- webapp/src/components/Table.tsx
+++ webapp/src/components/Table.tsx
@@ -1,36 +1,11 @@
-import { useEffect, useState } from "react";
-import { api } from "../api/api";
+import { Transaction } from "../App";
+import { formatCurrency } from "../utils/currencyFormatter";
 
-type Transaction = {
-  amount: number;
-  category: string;
-  description: string;
-  is_income: boolean;
-  date: string;
-  id: number;
-};
+interface ITable {
+  transactions: Transaction[];
+}
 
-export const Table = () => {
-  const [transactions, setTransactions] = useState<Transaction[]>([]);
-
-  const fetchTransactions = async () => {
-    const res = await api.get("/transactions/");
-    if (res.status === 200) {
-      setTransactions(res.data);
-    }
-  };
-
-  useEffect(() => {
-    fetchTransactions();
-  }, []);
-
-  const formatAmount = (amount: number) => {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-    }).format(amount);
-  };
-
+export const Table = ({ transactions }: ITable) => {
   return (
     <table className="table-fixed w-full text-left text-base rounded-lg">
       <thead>
@@ -38,18 +13,21 @@ export const Table = () => {
           <th className="p-2 border">Amount</th>
           <th className="p-2 border">Category</th>
           <th className="p-2 border">Description</th>
-          <th className="p-2 border">Income/Outcome</th>
+          <th className="p-2 border">Is income?</th>
           <th className="p-2 border">Date</th>
         </tr>
       </thead>
       <tbody>
         {transactions.map(
-          ({ amount, category, description, is_income, date }, index) => (
-            <tr className={`${index % 2 === 0 ? `bg-[#f0f2f6]` : `bg-white`}`}>
-              <td className="p-2 border">{formatAmount(amount)}</td>
+          ({ amount, category, description, is_income, date, id }, index) => (
+            <tr
+              className={`${index % 2 === 0 ? `bg-[#f0f2f6]` : `bg-white`}`}
+              key={id}
+            >
+              <td className="p-2 border">{formatCurrency(amount)}</td>
               <td className="p-2 border">{category}</td>
               <td className="p-2 border">{description}</td>
-              <td className="p-2 border">{is_income ? "income" : "outcome"}</td>
+              <td className="p-2 border">{is_income ? "No" : "Yes"}</td>
               <td className="p-2 border">{date}</td>
             </tr>
           )
