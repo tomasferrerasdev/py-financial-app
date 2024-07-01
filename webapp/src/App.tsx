029af52646d7e4228fef4195d8a8de60594e1a4e
Tomas Ferreras <hellotomasdev@gmail.com>
2024-06-30 22:25:24 -0300

    feat: add get transactions and transaction refresher

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,16 +1,44 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import { Form } from "./components/Form";
 import { Table } from "./components/Table";
+import { api } from "./api/api";
+import { Chart } from "./components/Chart";
+
+export type Transaction = {
+  amount: number;
+  category: string;
+  description: string;
+  is_income: boolean;
+  date: string;
+  id: number;
+};
 
 function App() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  const fetchTransactions = async () => {
+    const res = await api.get("/transactions/");
+    if (res.status === 200) {
+      setTransactions(res.data);
+    }
+  };
+
+  useEffect(() => {
+    fetchTransactions();
+  }, []);
+
   return (
     <main className="min-h-screen bg-white custom-container px-4 pt-24 pb-40">
       <div className="fixed w-full h-1 top-0 left-0 gradient" />
       <h1 className="font-sans font-bold text-gray-900 text-4xl mb-8">
-        My financial app
+        My anual financial app
       </h1>
-      <div className="flex flex-col gap-4 mb-10">{/* <Form /> */}</div>
-      <Table />
+      <div className="flex flex-col gap-8">
+        <Form refetch={fetchTransactions} />
+        <Table transactions={transactions} />
+        <Chart transactions={transactions} />
+      </div>
     </main>
   );
 }
