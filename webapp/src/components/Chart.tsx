4ce1a56505cd8e9abbf3951b3baad2f8b7d04956
Tomas Ferreras <hellotomasdev@gmail.com>
2024-07-01 00:19:43 -0300

    feat: improve user experience in chart

--- webapp/src/components/Chart.tsx
+++ webapp/src/components/Chart.tsx
@@ -42,31 +42,40 @@ export const Chart = ({ transactions }: IChart) => {
   };
 
   return (
-    <div className="w-full min-h-screen">
+    <div className="w-full">
       <h2 className="font-sans font-bold text-gray-900 text-2xl mb-2">
         Spend chart
       </h2>
-      <div className="w-full h-[300px]">
+      <div className="w-full h-[300px] relative">
         <ResponsiveContainer>
           <LineChart width={500} height={300} data={sortedData}>
             <CartesianGrid strokeDasharray="2 2" />
-            <XAxis dataKey="date" interval="preserveStartEnd" />
-            <Tooltip content={<CustomTooltip />} />
-            <Line
-              type="monotone"
-              dataKey="totalAmount"
-              stroke="url(#colorPv)"
-              activeDot={{ r: 6 }}
-            />
-            <defs>
-              <linearGradient id="colorPv" x1="0" y1="0" x2="1" y2="0">
-                <stop offset="0%" stopColor="rgba(131, 58, 180, 1)" />
-                <stop offset="50%" stopColor="rgba(253, 29, 29, 1)" />
-                <stop offset="100%" stopColor="rgba(252, 176, 69, 1)" />
-              </linearGradient>
-            </defs>
+            {transactions.length > 0 && (
+              <>
+                <XAxis dataKey="date" interval="preserveStartEnd" />
+                <Tooltip content={<CustomTooltip />} />
+                <Line
+                  type="monotone"
+                  dataKey="totalAmount"
+                  stroke="url(#colorPv)"
+                  activeDot={{ r: 6 }}
+                />
+                <defs>
+                  <linearGradient id="colorPv" x1="0" y1="0" x2="1" y2="0">
+                    <stop offset="0%" stopColor="rgba(131, 58, 180, 1)" />
+                    <stop offset="50%" stopColor="rgba(253, 29, 29, 1)" />
+                    <stop offset="100%" stopColor="rgba(252, 176, 69, 1)" />
+                  </linearGradient>
+                </defs>
+              </>
+            )}
           </LineChart>
         </ResponsiveContainer>
+        {transactions.length === 0 && (
+          <p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
+            No data available
+          </p>
+        )}
       </div>
     </div>
   );
