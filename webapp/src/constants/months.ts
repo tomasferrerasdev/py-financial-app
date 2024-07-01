79a08f7aa6635a6fa58eee1dfa9c3f3229c8693c
Tomas Ferreras <hellotomasdev@gmail.com>
2024-06-30 22:25:56 -0300

    feat: add constant maps

---
+++ webapp/src/constants/months.ts
@@ -0,0 +1,29 @@
+export const monthOrder: { [key: string]: number } = {
+  Jan: 1,
+  Feb: 2,
+  Mar: 3,
+  Apr: 4,
+  May: 5,
+  Jun: 6,
+  Jul: 7,
+  Aug: 8,
+  Sep: 9,
+  Oct: 10,
+  Nov: 11,
+  Dec: 12,
+};
+
+export const monthNames: { [key: string]: string } = {
+  Jan: "January",
+  Feb: "February",
+  Mar: "March",
+  Apr: "April",
+  May: "May",
+  Jun: "June",
+  Jul: "July",
+  Aug: "August",
+  Sep: "September",
+  Oct: "October",
+  Nov: "November",
+  Dec: "December",
+};
